4dca6ce0b32471b4e364dc45fe7321eac5d995d6
Ian-Se-Malaret <iansml777@gmail.com>
2025-06-05 16:16:25 -0400

    Add initial code and files

--- from kivy to you.py
+++ from kivy to you.py
@@ -1,8 +1,16 @@
+import kivy
+kivy.require('2.1.1')
+
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.lang import Builder
+
+# 👇 This line tells Kivy to load the .kv file from inside the kv/ folder
+Builder.load_file('kv/home.kv')
 
 class MyApp(App):
     def build(self):
-        return Label(text="Rawr")
+        return Label(text='Rawr~')  # you can remove this if you're building UI in .kv
 
-MyApp().run()
+if __name__ == '__main__':
+    MyApp().run()
